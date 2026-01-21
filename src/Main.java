dd701aad5606424a1394005fc15c4286dbffc876
Andrew Kuchinskiy <kucinskijandrej36@gmail.com>
2026-01-21 11:51:48 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,6 +5,9 @@ public class Main {
   public static void main(String[] args) {
     int n;
     int p = 0;
+    int bulls = 0;
+    int cows = 0;
+    int attemps = 0;
 
     Scanner scanner = new Scanner(System.in);
     Random r = new Random();
@@ -26,44 +28,63 @@ public class Main {
     }
 
     System.out.println("-------------------------------------------------------");
-    System.out.print("Напишить будь-яке " +n+"-значне число : ");
-    int b = scanner.nextInt();
 
-    //разбиваем числа на отдельные цифры и записываем их как массив
-    int[] digits = new int[n];
-    for (int i = n-1; i >= 0; i--) {
-      digits[i] = b % 10;
-      b /= 10;
-    }
-
-    int[] secretDigits = new int[n];
-    for (int i = n - 1; i >= 0; i--) {
-      secretDigits[i] = p % 10;
-      p /= 10;
-    }
 
     //Сравнение чисел и нахождение быков и коров
-    int bulls = 0;
-    int cows = 0;
+      while (bulls != n) {
+        bulls = 0;
+        cows = 0;
 
-    for (int i = 0; i < n; i++) {
-      if (digits[i] == secretDigits[i]) {
-        bulls++;
-      }
-    }
-
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        if (i != j && digits[i] == secretDigits[j]) {
-          cows++;
-          break;
+        System.out.print("Напишiть будь-яке " +n+"-значне число : ");
+        String asd = scanner.next();
+        if (asd.length() != n) {
+          System.out.println("Потрібно ввести рівно " + n + " цифр");
+          continue;
         }
-      }
+        //Преобразовуем строку в инт
+        int b = Integer.parseInt(asd);
+
+        //разбиваем числа на отдельные цифры и записываем их как массив
+        int[] digits = new int[n];
+        for (int i = n-1; i >= 0; i--) {
+          digits[i] = b % 10;
+          b /= 10;
+        }
+
+        int tempP = p;
+        int[] secretDigits = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+          secretDigits[i] = tempP % 10;
+          tempP /= 10;
+        }
+
+        for (int i = 0; i < n; i++) {
+          if (digits[i] == secretDigits[i]) {
+            bulls++;
+          }
+        }
+
+        for (int i = 0; i < n; i++) {
+          if (digits[i] == secretDigits[i]) continue;
+
+          for (int j = 0; j < n; j++) {
+            if (i != j && digits[i] == secretDigits[j]) {
+              cows++;
+              break;
+            }
+          }
+        }
+
+
+      System.out.println("Бики: " + bulls);
+      System.out.println("Корови: " + cows);
+      attemps++;
+        System.out.println("Кiлькiсть спроб: " +attemps);
+
+        if (bulls == n) {
+          System.out.println("🎉🎉Ви🎉вгадали🎉число🎉за🎉" + attemps + "🎉спроб!🎉🎉");
+        }
     }
-
-
-    System.out.println("Быки: " + bulls);
-    System.out.println("Коровы: " + cows);
 
     scanner.close();
   }
